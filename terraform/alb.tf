b35ea0a4a12f351a4d4ec59b59174473e7ae6cb2
Paripoornam Y <77569867+paripuranam@users.noreply.github.com>
2025-12-14 12:26:15 +0530

    Add AWS ALB and related resources configuration

---
+++ terraform/alb.tf
@@ -0,0 +1,35 @@
+resource "aws_lb" "main" {
+  name               = "${var.project_name}-alb"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.alb_sg.id]
+  # Spanning public subnets across AZs
+  subnets            = aws_subnet.public[*].id
+
+  enable_deletion_protection = false
+}
+
+resource "aws_lb_target_group" "app_tg" {
+  name     = "${var.project_name}-app-tg"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = aws_vpc.main.id
+
+  health_check {
+    path                = "/"
+    healthy_threshold   = 2
+    unhealthy_threshold = 10
+  }
+}
+
+# Listener for HTTP traffic
+resource "aws_lb_listener" "http" {
+  load_balancer_arn = aws_lb.main.arn
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.app_tg.arn
+  }
+}
